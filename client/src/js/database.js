1f261abb7ea8f5600df3f588b0b784ff6094b092
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-01-26 00:15:16 -0500

    I have defined StaleWhileRevalidate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,33 +15,33 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {console.log('putDb implemented');
 
-const jateDb= await openDB('jate',1);
+const jateDb= await openDB("jate",1);
 
-const tx= jateDb.transaction('jate','readwrite');
+const tx= jateDb.transaction("jate","readwrite");
 
-const store= tx.objectStore('jate');
+const store= tx.objectStore("jate");
 
 const request= store.put({jate:content});
 
 const result= await request;
 
-console.log('data saved to database', result);
+console.log("data saved to database", result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {console.log('getDb implemented');
 
-const getDb= await openDB('jate',1);
+const getDb= await openDB("jate",1);
 
-const getTx= getDb.transaction('jate','readonly');
+const getTx= getDb.transaction("jate","readonly");
 
-const getStore= getTx.objectStore('jate');
+const getStore= getTx.objectStore("jate");
 
 const getRequest= getStore.getAll();
 
 const getResult= await getRequest;
 
-console.log('All data is fetched', getResult);
+console.log("All data is fetched", getResult);
 }
 
 initdb();
